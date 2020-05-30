4a2b4aa5ce2218d723a399fa94529b32e28e14ec
younusraza909 <younusraza909@gmail.com>
2020-05-30 09:00:58 -0700

    Firebase Setup in App.js

--- src/App.js
+++ src/App.js
@@ -5,18 +5,41 @@ import { Route, Switch } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import { auth } from "./firebase/firebase.utils";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop" component={ShopPage} />
-        <Route exact path="/signin" component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      current: null,
+    };
+  }
+
+  unsubscribeFromAuth = null;
+  componentDidMount() {
+    //this is an open subscription between our firebase app and custom app
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ current: user });
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/shop" component={ShopPage} />
+          <Route exact path="/signin" component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
