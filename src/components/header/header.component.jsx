0ad9fef70c637747d2ba45d39d7516bd1dd2ec31
younusraza909 <younusraza909@gmail.com>
2020-06-02 08:42:18 -0700

    Used Connect and mapStateToProps function to get state from root reducers rateher than props

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,6 +5,9 @@ import { ReactComponent as Logo } from "../../assests/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
 //this is special syntax to import svg component
 
+import { connect } from "react-redux";
+//its hight order component function
+
 const Header = ({ currentUser }) => (
   <div className="header">
     <Link to="/" className="logo-container">
@@ -30,4 +33,11 @@ const Header = ({ currentUser }) => (
   </div>
 );
 
-export default Header;
+//in order to connect our component with state of root user
+
+const mapStateToProps = (state) => {
+  currentUser: state.user.currentUser;
+};
+
+export default connect(mapStateToProps)(Header);
+//connect(mapStateToProps) this return a fucntion in which we have to passed Header
