bd84bc54671ec22c2e532b44a63856b693b295b8
younusraza909 <younusraza909@gmail.com>
2020-06-03 08:14:00 -0700

    Updated shop data with new data ,updated cart utils to group items in cart reducer

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,17 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToAdd.id
+  );
+
+  //We always return new array just bcz react will re render component  if it will find new array if we only change array's object property it will not re render
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  //in this fucntion if it will run first time this part will run and quantity will attatch to object property
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
