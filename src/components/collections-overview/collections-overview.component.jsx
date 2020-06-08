6f9d1e43eff5fb4814a86c391ae2f147099e2f89
younusraza909 <younusraza909@gmail.com>
2020-06-07 18:44:46 -0700

    Changing Selector For Our Shop Overview Component

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -5,7 +5,7 @@ import { createStructuredSelector } from "reselect";
 
 import CollectionPreview from "../collection-preview/collection-preview";
 
-import { shopCollectionData } from "../../redux/shop/shop.selectors";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
 
 import "./collections-overview.styles.scss";
 
@@ -18,7 +18,7 @@ const CollectionsOverview = ({ collections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: shopCollectionData,
+  collections: selectCollectionsForPreview,
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -7,6 +7,12 @@ export const shopCollectionData = createSelector(
   (shop) => shop.collections
 );
 
+//in this selector we are just converting our object returned into an array for out some component
+export const selectCollectionsForPreview = createSelector(
+  [shopCollectionData],
+  (collections) => Object.keys(collections).map((key) => collections[key])
+);
+
 export const selectCollection = (collectionUrlParam) =>
   createSelector(
     [shopCollectionData],
