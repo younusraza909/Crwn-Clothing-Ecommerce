2854e6f9533255da611534e71175b4cfa3f9b602
younusraza909 <younusraza909@gmail.com>
2020-06-15 11:11:55 -0700

    Change With Spinner Function for scalabilty and to remove a bug

--- src/components/with-spinner/with-spinner.component.jsx
+++ src/components/with-spinner/with-spinner.component.jsx
@@ -3,14 +3,17 @@ import React from "react";
 import { SpinnerContainer, SpinnerOverlay } from "./with-spinner.style";
 
 //wrappedComponent is basically a component if there will be loading property there to true it eill return loading component else compoennt itself
-const WithSpinner = (WrappedComponent) => ({ isLoading, ...otherProps }) => {
-  return isLoading ? (
-    <SpinnerOverlay>
-      <SpinnerContainer />
-    </SpinnerOverlay>
-  ) : (
-    <WrappedComponent {...otherProps} />
-  );
+const WithSpinner = (WrappedComponent) => {
+  const Spinner = (isLoading, { ...otherProps }) => {
+    return isLoading ? (
+      <SpinnerOverlay>
+        <SpinnerContainer />
+      </SpinnerOverlay>
+    ) : (
+      <WrappedComponent {...otherProps} />
+    );
+  };
+  return Spinner;
 };
 
 export default WithSpinner;
